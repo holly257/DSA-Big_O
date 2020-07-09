f520e7e786473a32d2e5736ce82ec04240bdc0fc
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-07-09 18:51:30 -0400

    finished 12.3 and 12.4

--- app.js
+++ app.js
@@ -16,19 +16,39 @@ function countSheep(num) {
 }
 //countSheep(3);
 
-
 ////2. Power Calculator
 function powerCalculator(num, pow) {
-    if(pow < 0){
+    if (pow < 0) {
         return 'exponent should be >= 0';
     }
 
     let returnVal = num;
 
-    for(i=0; i<pow-1; i++){
-        returnVal = returnVal * num
+    for (i = 0; i < pow - 1; i++) {
+        returnVal = returnVal * num;
     }
     return returnVal;
 }
-
 //console.log(powerCalculator(10, 4))
+
+////3. Reverse String
+function stringRev(str) {
+    let newStr = '';
+    for (i = str.length; i > 0; i--) {
+        let newChar = str.charAt(i - 1);
+        newStr = ''.concat(newStr, newChar);
+    }
+    return newStr;
+}
+//console.log(stringRev('hello'));
+
+////4. nth Triangular Number
+function triNum(num) {
+    let sum = 0;
+    for(i=num; i>0; i--){
+        sum = sum + i;
+    }
+    return sum;
+}
+
+//console.log(triNum(6))
