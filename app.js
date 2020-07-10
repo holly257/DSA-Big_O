cb07b756b83cbdaebdaffc0c8c9216ddc94d07a9
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-07-10 16:46:35 -0400

    finished tower of hanoi

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+//11. Tower of Hanoi
+function moveNDisks(N, fromPeg, toPeg, extraPeg) {
+    // if there's only one disk to move, then just print the move required
+    if (N == 1) console.log('Move disk from Peg ' + fromPeg + ' to ' + toPeg);
+    else {
+        // otherwise, then call the function to move N-1 disks out of the way...
+        moveNDisks(N - 1, fromPeg, extraPeg, toPeg);
+        // then move 1 disk to the destination
+        moveNDisks(1, fromPeg, toPeg, extraPeg);
+        // and the N-1 that were out of the way should be brought back
+        moveNDisks(N - 1, extraPeg, toPeg, fromPeg);
+    }
+}
+
+// now, let's try a test case with 5 disks
+moveNDisks(3, 'A', 'B', 'C');
+
 //12. Solve the following with iteration
 ////1. Counting Sheep
 
